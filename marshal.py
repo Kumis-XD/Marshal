bb166ca5258fe8f162314cb5920037cb2ee86799
Kumis-XD <114278627+Kumis-XD@users.noreply.github.com>
2022-11-10 13:38:16 +0700

    Update marshal.py

--- marshal.py
+++ marshal.py
@@ -1,16 +1,44 @@
-import os,marshal
-
-os.system('clear')
-
-f = input('Masukan file yg mau di encript : ')
-o = input('Disimpan ke mana? : ')
-b = open(f, "r").read()
-
-c = compile(b, "", "exec")
-en = marshal.dumps(c)
-b2 = open(o, "w")
-
-b2.write("import marshal\n")
-b2.write("exec(marshal.loads"+repr(en)+"))")
-
-print(en)
+#!/usr/bin/python1.7
+#coding=utf-8
+import marshal,os,sys,time
+os.system("clear")
+aleju = ("""
+     ╔╦╗┌─┐┬─┐┌─┐┬ ┬┌─┐┬
+     ║║║├─┤├┬┘└─┐├─┤├─┤│
+     ╩ ╩┴ ┴┴└─└─┘┴ ┴┴ ┴┴─┘
+     Creator : Kumis-xd
+     Github : KUMIS-XD\n""")
+def aleeju():
+        try:
+                print (aleju)
+                print ('   [-] Example > /sdcard/file.py')
+                file = input ('   [+] File Kamu : ')
+                fileopen = open(file).read()
+                a = compile(fileopen, 'dg', 'exec')
+                m = marshal.dumps(a)
+                s = repr(m)
+                b = ('import marshal\nexec(marshal.loads(' + s +'))')
+                c = file.replace('.py', '.py')
+                d = open(c, 'w')
+                d.write(b)
+                d.close()
+                print ('   [+] Success > ',c)
+                time.sleep(1)
+                aq = input ('  [?] Ingin encrypt lagi? [y/n]')
+                if aq =="":
+                        print ('   Command not found !')
+                        os.sys.exit()
+                elif aq =="y" or aq =="Y":
+                        aleeju()
+                else:
+                        if aq =="n" or aq =="N":
+                                print ('   > Terimakasih anjeng ;v\n')
+                        else:
+                                print ('   Command not found !')
+                                os.sys.exit()
+        except IOError:
+                print ('   File Tidak Ditemukan ! ')
+                time.sleep(1)
+                aleeju()
+if __name__=='__main__':
+        aleeju()
